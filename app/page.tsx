eee92a66dd688845f4ea891e4d499abbfe75e88e
bishwa <bishwamishra6250@gmail.com>
2023-12-20 09:06:02 +0545

    initial commit1

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default function Home() {
+  const [allCars, setAllCars] = useState([]);
+  const [isDataEmpty, setIsDataEmpty] = useState(false);
 
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const cars = await fetchCars({});
+        setAllCars(cars || []);
+        setIsDataEmpty(!Array.isArray(cars) || cars.length === 0);
+      } catch (error) {
+        console.error("Error fetching cars:", error);
+        setIsDataEmpty(true);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <main className="overflow-hidden">
       <Hero />
-      <div className="mt-12 padding-x padding-y max-width" id="discover">
+      <div
+        className="mt-12 padding-x padding-y max-w-screen-xl mx-auto"
+        id="discover"
+      >
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
@@ -17,29 +35,24 @@ export default async function Home() {
 
         <div className="home__filters">
           <SearchBar />
-          <div className="home__filter-container">
-            {/* <CustomFilter title="fuel" />
-            <CustomFilter title="year" /> */}
-          </div>
+          {/* <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div> */}
         </div>
 
         {!isDataEmpty ? (
           <section>
-            <div className="home__cars-wrapper">
-              {allCars?.map((car) => (
-                <CarCard car={car} />
+            <div className="home__cars-wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {allCars.map((car, index) => (
+                <CarCard key={index} car={car} />
               ))}
             </div>
-
-            {/* <ShowMore
-              pageNumber={(searchParams.limit || 10) / 10}
-              isNext={(searchParams.limit || 10) > allCars.length}
-            /> */}
           </section>
         ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-            <p>{allCars?.message}</p>
+            <p>No cars available at the moment.</p>
           </div>
         )}
       </div>
--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
-import { CarProps, FilterProps } from "@types";
+// import { CarProps, FilterProps } from "@types";
+import { CarProps, FilterProps } from "@/types";
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50; // Base rental price per day in dollars
